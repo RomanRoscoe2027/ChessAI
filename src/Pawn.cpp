baa6cd3ab520090541eb3bb0239bd6bbdced86a7
RomanRoscoe2027 <roscoer1@msu.edu>
2025-08-29 09:33:38 -0700

    struct for pawn obstruction and double step there, gonna get started with bounds checking as well as obstruction logic in other pieces as well. Saving king for last, because the checking and pin logic seems like a nightmare.

--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -4,5 +4,32 @@ Pawn::Pawn(bool isWhite)
     : Piece(isWhite ? 'P' : 'p', isWhite, PieceType::PAWN){}
 
 bool Pawn::isValidMove(int startX, int startY, int endX, int endY, const Board& grid) const{
-    int start_rank = isWhite ? 0:7;
+    const int startRank = isWhite ? 0:7;
+    //Need dX and dY for simplicity in all classes to be honest, another change realized I should make
+    const int dX = endX - startX;
+    const int dY = endY-startY;
+    /*google and robots think a directionally variable important...
+    would certainly say so for long path movements where incrementing is necessary
+    to check all obstruction squares, will start using it with pawn for that reason.*/
+    const int dir = isWhite ? 1 : -1;
+    //define logic just for single step w obstruction
+
+    if(grid.inBounds(endX,endY) == false)
+
+    if (dir == 1  && dY ==1){
+        return(grid.isEmpty(endX,endY));
+    }
+    else if (dir == -1 && dY==-1){
+        return(grid.isEmpty(endX,endY));
+    }
+    //define logic for just double step w obstruction
+
+    if (dir == -1 && dY == -2 && startY ==startRank){
+        return(grid.isEmpty(endX,endY) && grid.isEmpty(endX+dir, endY+dir)); //check first sq too
+    }
+    else if (dir == 1 && dX ==2 && startY == startRank){
+        return(grid.isEmpty(endX,endY) && grid.isEmpty(endX+dir, endY+dir));//same
+    }
+    
+
 }
